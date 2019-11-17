da2ad086fd57e78e04dc19063fb272c6c0ce40d9
Alexander Moerman <a.moerman@dobcomed.com>
2019-11-17 14:31:53 +0100

    Use nuget instead of dotnet nuget in publish.cmd

--- publish.cmd
+++ publish.cmd
@@ -1,7 +1,7 @@
-SET nugetversion=0.2.1
+SET nugetversion=0.2.2
 dotnet pack ./src/DeadManSwitch/DeadManSwitch.csproj --configuration Release --include-source
 dotnet pack ./src/DeadManSwitch.AspNetCore/DeadManSwitch.AspNetCore.csproj --configuration Release --include-source
-dotnet nuget push ./src/DeadManSwitch/bin/Release/DeadManSwitch.%nugetversion%.nupkg --source nuget.org
-dotnet nuget push ./src/DeadManSwitch.AspNetCore/bin/Release/DeadManSwitch.AspNetCore.%nugetversion%.nupkg --source nuget.org
-dotnet nuget push ./src/DeadManSwitch/bin/Release/DeadManSwitch.%nugetversion%.nupkg --source Github
-dotnet nuget push ./src/DeadManSwitch.AspNetCore/bin/Release/DeadManSwitch.AspNetCore.%nugetversion%.nupkg --source Github
+nuget push ./src/DeadManSwitch/bin/Release/DeadManSwitch.%nugetversion%.nupkg -source nuget.org
+nuget push ./src/DeadManSwitch.AspNetCore/bin/Release/DeadManSwitch.AspNetCore.%nugetversion%.nupkg -source nuget.org
+nuget push ./src/DeadManSwitch/bin/Release/DeadManSwitch.%nugetversion%.nupkg -source Github
+nuget push ./src/DeadManSwitch.AspNetCore/bin/Release/DeadManSwitch.AspNetCore.%nugetversion%.nupkg -source Github
